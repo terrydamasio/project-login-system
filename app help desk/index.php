7cde3de32160a97b5008e2fce769aca1b3fc5204
Terry Damasio <terrydamasio610@gmail.com>
2023-08-28 14:02:47 -0300

    Atuaização portfolio

--- app help desk/index.php
+++ app help desk/index.php
@@ -1,60 +1,57 @@
-!
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>App Help Desk</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <style>
-      .card-login {
-        padding: 30px 0 0 0;
-        width: 350px;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
-
-  <body>
-
-    <nav class="navbar navbar-dark bg-dark">
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>App Help Desk</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <style>
+    .card-login {
+      padding: 30px 0 0 0;
+      width: 350px;
+      margin: 0 auto;
+    }
+  </style>
+</head>
+<body>
+  
+  <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
         <img src="logo.php" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
       </a>
-    </nav>
+  </nav>
 
-    <div class="container">    
-      <div class="row">
+  <div class="container">    
+    <div class="row">
 
-        <div class="card-login">
-          <div class="card">
-            <div class="card-header">
-              Login
-            </div>
-            <div class="card-body">
-              <form method="post" action="valida_login.php">
-                <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail" name="email">
-                </div>
-                <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Senha" name="senha">
-                </div>
-
-                  <?php  if(isset($_GET['login']) && $_GET['login'] == 'erro') { ?>
-                    <div class="text-danger">Usuário ou senha inválido(s)</div>
-                  <?php } ?>
-
-                  <?php  if(isset($_GET['login']) && $_GET['login'] == 'erroAcesso') { ?>
-                    <div class="text-danger">Faça o login para acessar as outras páginas.</div>
-                  <?php } ?>
-                  
-
-                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
-              </form>
-            </div>
+      <div class="card-login">
+        <div class="card">
+          <div class="card-header">
+            Login
           </div>
-        </div>
+        <div class="card-body">
+        
+        <form method="post" action="valida_login.php">
+          <div class="form-group">
+            <input type="email" class="form-control" placeholder="E-mail" name="email">
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" placeholder="Senha" name="senha">
+          </div>
+
+          <?php  if(isset($_GET['login']) && $_GET['login'] == 'erro') { ?>
+            <div class="text-danger">Usuário ou senha inválido(s)</div>
+          <?php } ?>
+
+          <?php  if(isset($_GET['login']) && $_GET['login'] == 'erroAcesso') { ?>
+            <div class="text-danger">Faça o login para acessar as outras páginas.</div>
+          <?php } ?>
+                    
+          <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
-  </body>
+  </div>
+</body>
 </html>
