70aa594337851dccf3ea12fd845b63537c5db852
Terry Damasio <terrydamasio610@gmail.com>
2023-08-30 18:01:37 -0300

    Atualização estudos php oo

--- Estudos PHP OO/index.php
+++ Estudos PHP OO/index.php
@@ -1,22 +1,32 @@
 <?php
 
-    class Pessoa {
-        const nome = "Terry"; //criar constante
-
-        public function exibirNome() {
-            echo self::nome; //referenciar a constante dentro do escopo da função na classe Pessoa
-        }
+    class Pedido {
+        public $numero;
+        public $cliente;
     }
 
-    class Terry extends Pessoa {
-        const nome = "Damasio";
-
-        public function exibirNome() {
-            echo parent::nome; //referenciar a constante dentro do escopo da função na classe pai (Pessoa)
-        }
+    class Cliente {
+        public $nome;
+        public $endereco;
     }
 
-    $terry = new Terry();
-    $terry->exibirNome();
+    $cliente = new Cliente();
+    $cliente->nome = "Terry Damasio Santos";
+    $cliente->endereco = "Rua xxx, 332 - Recife";
+
+    $pedido = new Pedido();
+    $pedido->numero = "123";
+    $pedido->cliente = $cliente;
+
+    $dados = [
+        'numero' => $pedido->numero,
+        'nome_cliente' => $pedido->cliente->nome,
+        'endereco_clietne' => $pedido->cliente->endereco
+    ];
+
+    var_dump($dados);
+
+
+
 
 ?>
