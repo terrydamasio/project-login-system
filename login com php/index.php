90b0d022c47b8bd265bea0f0d01569070ae1161d
Terry Damasio Santos <terry@lifecti.com.br>
2023-10-04 14:10:42 -0300

    shg

---
+++ login com php/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="loginModel.php" method="post">
+        <p>
+            <label for="">Email</label>
+            <input type="email" name="email">
+        </p>
+        <p>
+            <label for="">Senha</label>
+            <input type="password" name="senha">
+        </p>
+
+        <p>
+            <button type="submit">Entrar</button>
+        </p>
+    </form>
+        
+</body>
+</html>
---
+++ login com php/painel.php
@@ -0,0 +1,15 @@
+<?php 
+    require "loginModel.php"; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Painel</h1>
+    <p>Bem vindo ao painel</p>
+</body>
+</html>
